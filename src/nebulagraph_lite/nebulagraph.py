8d41c068a55ddeca820df87418288167e0a92284
Wey Gu <weyl.gu@gmail.com>
2024-01-15 14:49:48 +0800

    chore: fix which on ubuntu, add --allow-root for all

--- src/nebulagraph_lite/nebulagraph.py
+++ src/nebulagraph_lite/nebulagraph.py
@@ -62,7 +62,7 @@ class NebulaGraphLet:
         else:
             self._python_bin_path = os.path.dirname(os.sys.executable)
             result = subprocess.run(
-                f"{self._python_bin_path}/udocker --help",
+                f"{self._python_bin_path}/udocker --allow-root --help",
                 shell=True,
                 stdout=subprocess.PIPE,
                 stderr=subprocess.PIPE,
@@ -230,7 +230,7 @@ class NebulaGraphLet:
     def _try_shoot_service(self, service: str):
         try:
             self._run_udocker(
-                f"ps | grep {service} | awk '{{print $1}}' | xargs -I {{}} udocker rm -f {{}}"
+                f"ps | grep {service} | awk '{{print $1}}' | xargs -I {{}} udocker --allow-root rm -f {{}}"
             )
             # TODO: use psutil to kill the process
             os.system(f"killall nebula-{service} > /dev/null 2>&1")
@@ -459,7 +459,7 @@ class NebulaGraphLet:
         """
         if self.on_colab:
             self._run_udocker(
-                "ps | grep nebula | awk '{print $1}' | xargs -I {} udocker rm -f {}"
+                "ps | grep nebula | awk '{print $1}' | xargs -I {} udocker --allow-root rm -f {}"
             )
             self._try_shoot_all_services()
             return
